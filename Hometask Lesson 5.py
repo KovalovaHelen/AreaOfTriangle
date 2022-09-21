ccb6ca3350c4122f7cc5838d59eafcbe3c03a5b9
KovalovaHelen <113236482+KovalovaHelen@users.noreply.github.com>
2022-09-21 11:06:13 +0300

    Update №2

--- Hometask Lesson 5.py
+++ Hometask Lesson 5.py
@@ -1,13 +1,13 @@
 
-a = int(input('Enter the value of a: '))
-b = int(input('Enter the value of b: '))
-c = int(input('Enter the value of c: '))
-
-if a > 10 and b > 10 and c > 10 and a % 3 == 0 and b % 3 == 0:
-    print('yes')
-else:
-    print('no')
-exit()
+#a = int(input('Enter the value of a: '))
+#b = int(input('Enter the value of b: '))
+#c = int(input('Enter the value of c: '))
+#
+#if a > 10 and b > 10 and c > 10 and a % 3 == 0 and b % 3 == 0:
+#    print('yes')
+#else:
+#    print('no')
+#exit()
 
 
 
@@ -16,13 +16,13 @@ b = int(input('Enter the value of b: '))
 c = int(input('Enter the value of c: '))
 max = int('0')
 
-if a > b and a > c:
+if a >= b and a >= c:
     max = a
     print(max)
-elif b > a and b > c:
+elif b >= a and b >= c:
     max = b
     print(max)
-elif c > a and c > b:
+elif c >= a and c >= b:
     max = c
     print(max)
 exit()
