abe521fe2e11cc90dfd8e89c5e8046a6a71e0e31
KovalovaHelen <113236482+KovalovaHelen@users.noreply.github.com>
2022-09-23 14:30:08 +0300

    Update Hometask Lesson 6

---
+++ Hometask Lesson 6.py
@@ -0,0 +1,38 @@
+#"""
+#1.Вывести треугольник #1 с шириной N с помощью цикла while
+#"""
+#N = int(input('Enter number N : '))
+#i=1
+#
+#while i<N+1:
+#    print ("*"*N)
+#    N=N-1
+
+#"""
+#2.Вывести треугольник #2 с шириной N с помощью цикла while
+#"""
+#
+#N = int(input('Enter number N : '))
+#i=1
+#while i<=N:
+#    print("*"*i)
+#    i += 1
+
+#""""
+#3.Вывести треугольник #3 с шириной N:
+#"""
+#
+#N = int(input('Enter number N : '))
+#i=1
+#
+#for i in range(N,0,-1):
+#    print((i*'*').rjust(N))
+
+#
+#4.Вывести треугольник #4 с шириной N:
+#
+#
+#N = int(input('Enter number N : '))
+#i=1
+#for i in range(1, N):
+#    print((i*"*").rjust(N))
